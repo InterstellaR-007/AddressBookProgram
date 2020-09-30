b392fb3271e4094621552b0b24844a73fac152b2
InterstellaR-007 <intrstelr.007@gmail.com>
2020-09-30 23:42:12 +0530

    [Anuj] Creating Contacts on Main class

--- AddressBookProgram/AddressBookMain.cs
+++ AddressBookProgram/AddressBookMain.cs
@@ -6,7 +6,18 @@ namespace AddressBookProgram
     {
         static void Main(string[] args)
         {
+            String[] person_Details = new string[8];
             Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("Enter the Contact details of a person : (fn,ln,addr,city,state,zip,phn,id");
+            string contact_Input = Console.ReadLine();
+            person_Details = contact_Input.Split(',');
+            /*foreach(var i in person_Details)
+            {
+                Console.WriteLine(i);
+            }*/
+
+
+            
         }
     }
 }
